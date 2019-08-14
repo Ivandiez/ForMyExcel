593297fdabe8593667838816fc79dfed458c2578
Ivan <39994401+Ivandiez@users.noreply.github.com>
2019-08-14 21:02:56 +0300

    Добавьте файлы проекта.

---
+++ ForMyExcel/ExcelClass.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace ForMyExcel
+{
+    class ExcelClass
+    {
+        readonly string path = "";   // путь к файлу
+        readonly _Application excel = new _Excel.Application();
+        Workbook wb;    // переменная для файла xlsx
+        Worksheet ws;   // переменная для листа excel
+
+        /// <summary>
+        /// Конструктор для класса Excel без параметров
+        /// </summary>
+        public ExcelClass()
+        {
+
+        }
+
+        /// <summary>
+        /// Конструктор для класса Excel с параметрами
+        /// </summary>
+        /// <param name="path">путь к файлу</param>
+        /// <param name="Sheet">лист</param>
+        public ExcelClass(string path, int Sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[Sheet];
+        }
+
+        /// <summary>
+        /// Чтение нескольких ячеек из диапазона
+        /// </summary>
+        /// <param name="starti">начальное значение строки</param>
+        /// <param name="starty">начальное значение столбца</param>
+        /// <param name="endi">конечное значение строки</param>
+        /// <param name="endy">конечное значение столбца</param>
+        /// <returns>содержимое ячеек из диапазона</returns>
+        public double[,] ReadRange(int starti, int starty, int endi, int endy)
+        {
+            Range range = (Range)ws.Range[ws.Cells[starti, starty], ws.Cells[endi, endy]];
+            var holder = range.Value2;
+            double[,] returndouble = new double[endi - starti + 1, endy - starty + 1];
+            for (int p = 1; p <= endi - starti + 1; p++)
+                for (int q = 1; q <= endy - starty + 1; q++)
+                    returndouble[p - 1, q - 1] = holder[p, q];
+            return returndouble;
+        }
+
+        /// <summary>
+        /// Запись в диапазон ячеек
+        /// </summary>
+        /// <param name="starti">начальное значение строки</param>
+        /// <param name="starty">начальное значение столбца</param>
+        /// <param name="endi">конечное значение строки</param>
+        /// <param name="endy">конечное значение столбца</param>
+        /// <param name="writestring">Содержимое ячеек, которое будет записано</param>
+        public void WriteRange(int starti, int starty, int endi, int endy, double[,] writestring)
+        {
+            Range range = (Range)ws.Range[ws.Cells[starti, starty], ws.Cells[endi, endy]];
+            range.Value2 = writestring;
+        }
+
+        public void ClearRange()
+        {
+            Range range = ws.Range[ws.Cells[7, 3], ws.Cells[37, 5]];
+            range.ClearContents();
+        }
+
+        /// <summary>
+        /// Закрытие процесса excel
+        /// </summary>
+        public void Close()
+        {
+            wb.Close();
+        }
+
+        /// <summary>
+        /// Сохранение файла excel по умолчанию в том же файле
+        /// </summary>
+        public void Save()
+        {
+            wb.Save();
+        }
+
+        /// <summary>
+        /// Сохранение файла excel в какой-то новый файл
+        /// </summary>
+        /// <param name="path">путь к новому файлу</param>
+        public void SaveAs(string path)
+        {
+            wb.SaveAs(path);
+        }
+
+    }
+}
---
+++ ForMyExcel/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForMyExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ExcelClass ex = new ExcelClass(@"C:\Users\vanka\source\repos\ForMyExcel\ForMyExcel\bin\Debug\Форма для ответа — копия1.xlsx", 1);
+
+            double[,] read = ex.ReadRange(7, 6, 38, 6);
+            ex.Close();
+            ExcelClass ex1 = new ExcelClass(@"C:\Users\vanka\source\repos\ForMyExcel\ForMyExcel\bin\Debug\Форма для ответа — копия1 — копия.xlsx", 1);
+            ex1.WriteRange(7, 7, 38, 7, read);
+            ex1.ClearRange();
+            ex1.Save();
+            ex1.Close();
+        }
+    }
+}
